f6034fe586bdae6edbc506babcf87081d468c67a
dr6g1 <35386414+dr6g1@users.noreply.github.com>
2022-01-14 09:13:30 +0100

    Update photoresize.sh

--- photoresize.sh
+++ photoresize.sh
@@ -2,9 +2,25 @@
 # zmniejsz główne zdjęcia do 580x
 echo -n "podaj ścieżkę do folderu zdjęć: "
 read maindir
-for file in $(find "$maindir"/ -name *.jpg)
-    do 
-        convert -resize 580x "$file" "$file"
+find "$maindir" -type d -name "* *" | rename 's/ /_/g'
+find "$maindir" -type f -name "* *" | rename 's/ /_/g'
+
+for directory in $(find "$maindir"/ -maxdepth 1 -type d -printf "%f\n")
+    do  
+    licz=0
+        for file in $(find "$maindir"/"$directory"/ -maxdepth 1 -name "*.png" -printf "%f\n")
+        do 
+            convert "$maindir"/"$directory"/"$file" "$maindir"/"$directory"/"$file".jpg
+            rm "$maindir"/"$directory"/"$file"
+        done
+
+        for file in $(find "$maindir"/"$directory"/ -maxdepth 1 -name "*.jpg" -printf "%f\n")
+        do 
+        licz=$((licz + 1))
+            convert -resize 580x "$maindir"/"$directory"/"$file" "$maindir"/"$directory"/"$licz".jpg
+            #echo $(pwd)
+        rm "$maindir"/"$directory"/"$file"
+        done
     done
 
 # stwórz katalogi na miniatury
@@ -12,14 +28,14 @@ for directory in $(find "$maindir"/ -type d)
     do 
         mkdir "$directory"/m
     done
-
 # tworzenie miniatur w katalogu m 
 for directory in $(find "$maindir"/ -maxdepth 1 -type d -printf "%f\n")
     do  
         for file in $(find "$maindir"/"$directory"/ -maxdepth 1 -name "*.jpg" -printf "%f\n")
         do 
-            convert -resize 90x "$maindir"/"$directory"/"$file" "$maindir"/"$directory"/m/"$file"
+                    convert -resize 90x "$maindir"/"$directory"/"$file" "$maindir"/"$directory"/m/"$file"
             #echo $(pwd)
         done
     done 
+find "$maindir" -empty -delete
 
