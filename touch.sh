1a97274b8acc314c5e7b0e2c252c5f12d7947ca1
dr6g1 <35386414+dr6g1@users.noreply.github.com>
2022-02-02 08:56:00 +0100

    Update touch.sh

--- touch.sh
+++ touch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 mount -t drvfs '\\192.168.5.3\nas\Wysyłka - skaner\' /mnt/skaner
-dir="202111"
+dir="202202"
 for i in {01..31}; do
     mkdir ./"$dir$i"
-    touch ./$dir$i/AW.txt ./$dir$i/PS.txt ./$dir$i/OP.txt ./$dir$i/AG.txt ./$dir$i/JG.txt ./$dir$i/LT.txt ./$dir$i/DR.txt ./$dir$i/NP.txt ./$dir$i/JK.txt
+    touch ./$dir$i/AW.txt ./$dir$i/PS.txt ./$dir$i/W.txt ./$dir$i/O.txt ./$dir$i/JG.txt ./$dir$i/LT.txt ./$dir$i/NP.txt ./$dir$i/JK.txt
 done
