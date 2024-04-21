27e3449008d58c4a93729e592ea9f160b28717f7
0x0465 <35386414+0x0465@users.noreply.github.com>
2024-04-21 20:58:07 +0200

    Update alienvault.sh
    update

--- alienvault.sh
+++ alienvault.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 #
-# Check IP from Mikrotiks router "IP / Firewall / Connections" in Alienvault (no. of pulses)
-# pdrzew
-#
+# Check IP reputaion on Alienvault OTX
+# by pdr
+# @0x2e8b
 
 
-
-# SSH to router, save connections to file
-file=router.txt
-ssh auto@IP -p 2323 ip firewall connection print > $file
-
-# Process file router.txt, sort and uniq IPs
-awk -F: '{ print $2 }' $file | awk -F' ' '{ print $2 }' | sort | uniq > ip.txt
+# Process file ip.txt, every IP in new line, file should be in the same directory as script
 lines=$(cat ip.txt)
 
 # Show number of unique IPs
@@ -23,17 +17,16 @@ echo -e "Pulses \t : IP \t \t \t : ASN"
 
 # Loop through IPs
 for line in $lines
-    do
+        do
 # Curl to Alienvault, save to file
-	json=$(curl https://otx.alienvault.com/api/v1/indicators/IPv4/$line/general 2>/dev/null)
-    echo $json > ip_alien.txt
+                json=$(curl https://otx.alienvault.com/api/v1/indicators/IPv4/$line/general 2>/dev/null)
+                    echo $json > ip_alien.txt
 # Extract usable fields from file
-    reputation=$(jq -r '.pulse_info.count' ip_alien.txt)
-    asn=$(jq -r '.asn' ip_alien.txt)
+                reputation=$(jq -r '.pulse_info.count' ip_alien.txt)
+                asn=$(jq -r '.asn' ip_alien.txt)
 # Loop: If IPs rep > 0, print rep, IP and ISP name.
-    if [[ $reputation -ne "0" ]]
-        then
-        echo -e "$reputation \t : $line \t : $asn:"
-    fi
-    done
-rm router.txt ip_alien.txt
+                if [[ $reputation -ne "0" ]]
+                    then
+                    echo -e "$reputation \t : $line \t : $asn:"
+                fi
+                done
