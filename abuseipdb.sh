bfe8171d398ea974d99d150542f52d3d5afe829c
0x0465 <35386414+0x0465@users.noreply.github.com>
2024-04-21 20:56:22 +0200

    Update abuseipdb.sh
    update

--- abuseipdb.sh
+++ abuseipdb.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
 #
-# Check IPs from Mikrotiks router "IP / Firewall / Connections" in AbuseIpDB
-# pdrzew
-#
-
+# Check IPs "IP / Firewall / Connections" in AbuseIpDB
+# by pdr
+# @0x2e8b
 api={EDITED}
-
-# SSH to router, save connections to file
-file=router.txt
-ssh auto@IP -p 2323 ip firewall connection print > $file
-
-# Process file router.txt, sort and uniq IPs
-awk -F: '{ print $2 }' $file | awk -F' ' '{ print $2 }' | sort | uniq > ip.txt
+# Put list of IPs in the ip.txt file, file should be in the same folder as script. Every IP in new line. 
 lines=$(cat ip.txt)
 
 # Show number of unique IPs
@@ -23,17 +16,16 @@ echo -e "Score \t : IP \t \t \t : ISP"
 
 # Loop through IPs
 for line in $lines
-    do
+        do
 # Curl to AbuseIPDB, save to file
-	json=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=$line" -d maxAgeInDays=10 -H "Key: $api" -H "Accept: application/json" 2>/dev/null)
-	echo "$json" > json.txt
+        json=$(curl -G https://api.abuseipdb.com/api/v2/check --data-urlencode "ipAddress=$line" -d maxAgeInDays=10 -H "Key: $api" -H "Accept: application/json" 2>/dev/null)
+            echo "$json" > json.txt
 # Extract usable fields from file
-	score=$(jq -r '.data.abuseConfidenceScore' json.txt)
-	isp=$(jq -r '.data.isp' json.txt)
+        score=$(jq -r '.data.abuseConfidenceScore' json.txt)
+        isp=$(jq -r '.data.isp' json.txt)
 # Loop: If IPs score > 0, print score, IP and ISP name.
-	if [[ $score -ne "0" ]]
-        then
-	   echo -e "$score \t : $line \t : $isp"
-	fi
-    done
-	rm router.txt json.txt
+        if [[ $score -ne "99999" ]]
+            then
+           echo -e "$score \t : $line \t : $isp"
+        fi
+        done
