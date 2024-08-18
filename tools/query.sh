c43a7cdf46d527d4b7271c4d2bd3e6c54299c85d
0x0465 <35386414+0x0465@users.noreply.github.com>
2024-08-18 10:54:40 +0200

    Update query.sh

--- tools/query.sh
+++ tools/query.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
-read -n 1 -p "database to search: global, fb, polish, All? (g/f/p/A) " ans;
+read -n 1 -p "database to search: global, facebook, polish, All? (g/f/p/A) " ans;
 echo ""
 case $ans in
         g|G)
                 echo "query:"
                 read query
                 echo ""
-                echo "searching in global:"
+                echo "searching global:"
                 zgrep -iraE --color "$query" ./global;;
 
         f|F)
                 echo "query:"
                 read query
-                echo "searching in Facebook:"
-                7z x -so fb.7z | egrep -i --color "$query";;
+                echo""
+                echo "searching facebook:"
+                7z x -so Facebook_BF.7z | egrep -i --color "$query";;
         p|P)
                 echo "query:"
                 read query
-                echo "searching in pl:"
+                echo ""
+                echo "searching polish:"
                 egrep -ir --color "$query" ./pl;;
         a|A)
                 echo "query:"
                 read query
                 echo ""
-                echo "searching in global:"
+                echo "searching global:"
                 zgrep -iraE --color "$query" ./global
+                echo ""
                 echo "--------------------------"
-                echo "searching in Facebook:"
-                7z x -so fb.7z | egrep -i --color "$query"
+                echo""
+                echo "searching facebook:"
+                7z x -so Facebook_BF.7z | egrep -i --color "$query"
+                echo ""
                 echo "---------------------------"
-                echo "searching in polish:"
+                echo ""
+                echo "searching polish:"
                 egrep -ir --color "$query" ./pl
                 echo ""
 esac
